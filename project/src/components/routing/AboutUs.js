69cf6312e0c423e1c6e88f0a1f91db950507d8fd
AaronC <91234889+AaronCWH@users.noreply.github.com>
2021-12-08 21:36:52 +0800

    test
    test push 2

--- project/src/components/routing/AboutUs.js
+++ project/src/components/routing/AboutUs.js
@@ -3,19 +3,6 @@ import { Routes, Route, Link } from "react-router-dom";
 function AboutUs() {
     return (
         <>
-<<<<<<< Updated upstream
-            <h1>About the Team!</h1>
-            <h3>
-                    HELLO WE ARE SKILLSUNION COHORT 2 GROUP 2!
-            </h3>
-            <p>
-                    We have been taking the Software Developer Immersive course for 11 weeks and 
-                    this web application is the fruit of what we've learned so far!
-                    <br/>
-                    We hope our karaoke partner have brought you as much laughter as it did for us!
-                    Do check out our song recommendations too :)
-            </p>
-=======
             <div className='about-container'>
                 <h2 className='about-header'>About the Team!</h2>
                 <h3 className='title'>
@@ -32,7 +19,6 @@ function AboutUs() {
                     </p>
                 </div>
             </div>
->>>>>>> Stashed changes
         </>
     )
 }

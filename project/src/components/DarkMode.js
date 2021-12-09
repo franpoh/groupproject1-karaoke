53d6ec47a5572380e82d374aec6542e365093cee
AaronC <91234889+AaronCWH@users.noreply.github.com>
2021-12-09 19:19:09 +0800

    CSS updates

--- project/src/components/DarkMode.js
+++ project/src/components/DarkMode.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import xmasImage from '../assets/christmas-1920.jpg'
+import xmasImage2 from '../assets/christmas-dark-1920.jpg'
 
 function DarkMode() {
 
@@ -14,20 +15,20 @@ function DarkMode() {
         if (darkMode === day) {
             setdarkMode(night);
             setcssState('night'); // css classname night with dark background / white font 
-            document.getElementById("mainbody").style.backgroundImage = "none";
-            document.body.style.backgroundImage = "none";
+            document.getElementById("mainbody").style.backgroundImage = `url(${xmasImage})`;
             document.body.style.backgroundColor = "white";
         } else if (darkMode === night) {
             setdarkMode(xmas);
             setcssState('xmas');
             document.getElementById("mainbody").style.backgroundImage = "none";
             document.body.style.backgroundImage = "none";
-            document.body.style.backgroundColor = "black";
+            document.body.style.backgroundColor = "rgba(26, 26, 26)";
         } else if (darkMode === xmas) {
             setdarkMode(day);
             setcssState('day');
-            document.getElementById("mainbody").style.backgroundImage = `url(${xmasImage})`;
-            document.body.style.backgroundImage = `url(${xmasImage})`;
+            document.getElementById("mainbody").style.backgroundImage = `url(${xmasImage2})`;
+            document.body.style.backgroundColor = "rgba(26,26,26)";
+            // document.body.style.backgroundImage = `url(${xmasImage})`; //removed this line in order to have none-repeated image.
         }
         document.getElementById("mainbody").className = `webpage ${cssState}`; // add class, not override
         document.body.style.className = `webpage ${cssState}`;
